91675fa36a4a9e7fa14ee3ac1de77976056e89e5
Thomas Silvers <thomassilvers@DN0a230057.SUNet>
2019-07-30 11:46:36 -0700

    build app

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import slack
+import ssl as ssl_lib
+import certifi
+from onboarding_tutorial import OnboardingTutorial
+
+onboarding_tutorials_sent = {}
+
+def start_onboarding(web_client: slack.WebClient, user_id: str, channel: str):
+    # Create a new onboarding tutorial.
+    onboarding_tutorial = OnboardingTutorial(channel)
+
+    # Get the onboarding message payload
+    message = onboarding_tutorial.get_message_payload()
+
+    # Post the onboarding message in Slack
+    response = web_client.chat_postMessage(**message)
+
+    # Capture the timestamp of the message we've just posted so
+    # we can use it to update the message after a user
+    # has completed an onboarding task.
+    onboarding_tutorial.timestamp = response["ts"]
+
+    # Store the message sent in onboarding_tutorials_sent
+    if channel not in onboarding_tutorials_sent:
+        onboarding_tutorials_sent[channel] = {}
+    onboarding_tutorials_sent[channel][user_id] = onboarding_tutorial
