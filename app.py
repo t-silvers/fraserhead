142211c627fa6a415974355213f577893a5ed80e
Thomas Silvers <thomassilvers@DN0a230057.SUNet>
2019-07-30 15:24:41 -0700

    -> payload["data"].get("subtype")

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def update_thread(**payload):
     """Update onboarding welcome message after receiving a "pin_added"
     event from Slack. Update timestamp for welcome message as well.
     """
-    subtype = payload["data"]["subtype"]
+    subtype = payload["data"].get("subtype")
 
     if subtype == "message_replied":
         print('yay')
