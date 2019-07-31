b6b497bd110f31b33e9ce2ee12440cd463787bad
Thomas Silvers <thomassilvers@DN0a230057.SUNet>
2019-07-31 12:41:31 -0700

    final message text format

--- app.py
+++ app.py
@@ -307,7 +307,7 @@ def wiki_done(tutorial, channel):
             client = slack.WebClient(token=slack_token)
             client.chat_postMessage(
               channel=channel,
-              text=":champagne: You're done! Don't hesitate to reach out if you have more questions.\n Welcome to Fraser Lab!"
+              text=f":champagne: You're done! Don't hesitate to reach out if you have more questions.\n *Welcome to Fraser Lab!*"
             )
 
             # Update progress on tutorial
